c95dc0b4dfe77f34cea448ea1ea99b9dc64db6ce
ChuongPT_Desktop <phamtienchuong2000@gmail.com>
2023-05-09 16:53:40 +0700

    create Tank class, add rotate tank function

---
+++ Assets/Game/Scripts/Tank.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    private Rigidbody2D m_rigidbody => GetComponent<Rigidbody2D>();
+    public float rotationSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Tank controll
+        RotateTank();
+    }
+
+    void RotateTank()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow))
+            return;
+
+        float rotarion = m_rigidbody.rotation;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotarion += -1 * rotationSpeed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotarion += rotationSpeed;
+        }
+        rotarion %= 360f;
+        m_rigidbody.rotation = rotarion;
+    }
+}
