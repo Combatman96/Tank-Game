ad377e9992e678731d3af89f8dc2685803ee64a4
ChuongPT_Desktop <phamtienchuong2000@gmail.com>
2023-05-09 17:30:48 +0700

    add function tank can now shoot bullet

---
+++ Assets/Game/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D m_rigidbody => GetComponent<Rigidbody2D>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetVelocity(Vector3 velocity)
+    {
+        m_rigidbody.velocity = velocity;
+    }
+}
--- Assets/Game/Scripts/Tank.cs
+++ Assets/Game/Scripts/Tank.cs
@@ -6,6 +6,10 @@ public class Tank : MonoBehaviour
 {
     private Rigidbody2D m_rigidbody => GetComponent<Rigidbody2D>();
     public float rotationSpeed = 20f;
+    public float moveSpeed = 5f;
+    public float bulletSpeed = 2f;
+    [SerializeField] private Transform m_bulletSpawnPoint;
+    [SerializeField] private Bullet m_bulletPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -14,10 +18,13 @@ public class Tank : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        //Tank controll
+        // Tank controll
+        // NOTE: If you want true tank controll the tank can not rotate and move at the same time.
         RotateTank();
+        MoveTank();
+        ShootBullet();
     }
 
     void RotateTank()
@@ -26,15 +33,31 @@ public class Tank : MonoBehaviour
             return;
 
         float rotarion = m_rigidbody.rotation;
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rotarion += -1 * rotationSpeed;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rotarion += rotationSpeed;
-        }
+        if (Input.GetKey(KeyCode.RightArrow)) rotarion += -1 * rotationSpeed;
+        if (Input.GetKey(KeyCode.LeftArrow)) rotarion += rotationSpeed;
         rotarion %= 360f;
         m_rigidbody.rotation = rotarion;
+    }
+
+    void MoveTank()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.DownArrow))
+            return;
+        int forward = 0;
+        if (Input.GetKey(KeyCode.UpArrow)) forward = 1;
+        if (Input.GetKey(KeyCode.DownArrow)) forward = -1;
+        Vector3 velocity = transform.up * moveSpeed * forward;
+        m_rigidbody.velocity = velocity;
+    }
+
+    void ShootBullet()
+    {
+        // Note: Just a thought, we can only shoot one bullet every n seconds.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Bullet bullet = Instantiate(m_bulletPrefab, m_bulletSpawnPoint.position, Quaternion.identity);
+            Vector3 velocity = m_bulletSpawnPoint.up * bulletSpeed;
+            bullet.SetVelocity(velocity);
+        }
     }
 }
